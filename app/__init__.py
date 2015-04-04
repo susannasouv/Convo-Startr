6932a2450d33f7094534005877e57b4add7b5870
susannasouv <ssouv@berkeley.edu>
2015-04-04 10:33:43 -0700

    Changes that match needs and intentions
    Also comments and whatnot

--- app/__init__.py
+++ app/__init__.py
@@ -3,5 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object('config')
 
-from app import views
+# purpose: creates the application object(of class Flask)
+
+from app import views # avoids circular references
 
