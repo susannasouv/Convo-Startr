8707e05c533a86e8ffe017314dffc186cb4ff0b7
susannasouv <ssouv@berkeley.edu>
2015-04-05 11:48:31 -0700

    Using php instead

--- app/views.py
+++ app/views.py
@@ -1,8 +1,23 @@
 from flask import render_template
 from app import app
 
+import random
+
+import sqlite3
+conn = sqlite3.connect('test.db')
+c = conn.cursor()
+
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    return render_template('index.html') # more arguments fill in {{ ... }}
 
+def response(name, relationship, why):
+	responses = []
+	keywords = get_keywords(why) # Python list of keywords
+	for keyword in keywords:
+		c.execute("SELECT response FROM " + relationship + " WHERE reason = " + keyword + " ;")
+		responses.append(c.fetchone())
+	if responses == []:
+		return name + "...hi"
+	return random.choice(responses)
